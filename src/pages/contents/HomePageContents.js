97a96df58fa07d447f7bcc5202a18ffa95f3cfcd
shahid98 <Shahidreza98@gmail.com>
2021-04-04 09:55:06 +0530

    Update HomePageContents.js

--- src/pages/contents/HomePageContents.js
+++ src/pages/contents/HomePageContents.js
@@ -6,50 +6,7 @@ import {
 
 const HomePageContent = (
   <SupportingContentText>
-    Hello&#128075; My name is Dan, and I'm a Senior at{" "}
-    <a
-      href="https://www.davidson.edu/"
-      target="_blank"
-      rel="noreferrer"
-      style={{
-        color: "#748592",
-        textDecoration: "underline",
-        textDecorationColor: "#E6E6E6",
-        fontWeight: "bold",
-      }}
-      onMouseEnter={(e) => {
-        navLinkStylesEnter(e, "underline", "2px", "#525252", "#748492");
-      }}
-      onMouseLeave={(e) => {
-        navLinkStylesLeave(e, "none", "#748492");
-      }}
-    >
-      Davidson College
-    </a>
-    . I'm an Economics major, and I like to spend my time helping companies build better businesses and software, reading, and
-    working on{" "}
-    <a
-      href="/projects"
-      target="_blank"
-      rel="noreferrer"
-      style={{
-        color: "#748592",
-        textDecoration: "underline",
-        textDecorationColor: "#E6E6E6",
-        fontWeight: "bold",
-      }}
-      onMouseEnter={(e) => {
-        navLinkStylesEnter(e, "underline", "2px", "#525252", "#748492");
-      }}
-      onMouseLeave={(e) => {
-        navLinkStylesLeave(e, "none", "#748492");
-      }}  
-    >
-      coding projects
-    </a>
-    . I'm passionate about contributing to Open Source Software, designing
-    accessible and easy-to-use Interfaces, and building a more equitable
-    future for technologists.
+   THIS IS DONE  BY US MAKATIAN
     <br />
     <br />
     Learn more{" "}
@@ -73,25 +30,7 @@ const HomePageContent = (
       about me
     </a>{" "}
     and the things I'm{" "}
-    <a
-      href="/projects"
-      target="_blank"
-      rel="noreferrer"
-      style={{
-        color: "#748592",
-        textDecoration: "underline",
-        textDecorationColor: "#E6E6E6",
-        fontWeight: "bold",
-      }}
-      onMouseEnter={(e) => {
-        navLinkStylesEnter(e, "underline", "2px", "#525252", "#748492");
-      }}
-      onMouseLeave={(e) => {
-        navLinkStylesLeave(e, "none", "#748492");
-      }}
-    >
-      working on
-    </a>
+   
     !
   </SupportingContentText>
 );
